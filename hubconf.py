048c93a63d19914e1ebb455eaa217eb29cbe7e3b
Nikos Karavasilis <nikoskaravasilis10@gmail.com>
2023-04-26 14:50:12 +0200

    small fix

--- hubconf.py
+++ hubconf.py
@@ -157,7 +157,7 @@ def create_model(model_name, class_names=CLASS_NAMES, device=DEVICE,
             str(PATH_YOLOv6/'weights'))
     return Detector(str(PATH_YOLOv6/'weights') + f'/{model_name}.pt',
                     class_names, device, img_size=img_size, conf_thres=conf_thres,
-                    iou_thres=iou_thres, max_det=max_det, stride=stride, fuse=False)
+                    iou_thres=iou_thres, max_det=max_det, stride=stride, fuse=fuse)
 
 
 def yolov6n(class_names=CLASS_NAMES, device=DEVICE, img_size=640, conf_thres=0.25, iou_thres=0.45, max_det=1000):
