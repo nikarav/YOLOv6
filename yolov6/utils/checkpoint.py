46b53d6d1e9532540da506e227f23df2ea7d5ce0
Nikos Karavasilis <nikoskaravasilis10@gmail.com>
2023-04-26 15:37:52 +0200

    small fix

--- yolov6/utils/checkpoint.py
+++ yolov6/utils/checkpoint.py
@@ -25,6 +25,7 @@ def load_checkpoint(weights, map_location=None, inplace=True, fuse=False):
     ckpt = torch.load(weights, map_location=map_location)  # load
     model = ckpt['ema' if ckpt.get('ema') else 'model'].float()
     if fuse:
+        LOGGER.info(f"fuse={fuse}")
         LOGGER.info("\nFusing model...")
         model = fuse_model(model).eval()
     else:
